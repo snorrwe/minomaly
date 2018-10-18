39ae5585e8038bde8750b353f4ae88da476557d4
Daniel Kiss <littlesnorrboy@gmail.com>
2018-10-19 00:12:07 +0200

    Add initial Matrix implementation

---
+++ include/minomaly/common/matrix.hpp
@@ -0,0 +1,165 @@
+#pragma once
+#include <array>
+#include <cassert>
+
+namespace mino
+{
+template <uint16_t _cols, uint16_t _rows, typename TValue = float>
+class Matrix
+{
+
+public:
+    using Self = Matrix<_cols, _rows, TValue>;
+    using ValueContainer = std::array<TValue, _cols * _rows>;
+    using ValuesIterator = typename ValueContainer::iterator;
+    using ValuesConstIterator = typename ValueContainer::const_iterator;
+
+    constexpr Matrix() = default;
+    explicit constexpr Matrix(std::array<TValue, (_cols * _rows)> const& values) : values(values)
+    {
+    }
+    explicit constexpr Matrix(std::array<TValue, (_cols * _rows)>&& values)
+        : values(std::move(values))
+    {
+    }
+    constexpr Matrix(Matrix const&) = default;
+    constexpr Matrix(Matrix&&) = default;
+    ~Matrix() = default;
+    constexpr Matrix& operator=(Matrix const&) = default;
+    constexpr Matrix& operator=(Matrix&&) = default;
+
+    constexpr auto columns() const -> uint16_t;
+    constexpr auto rows() const -> uint16_t;
+    constexpr auto size() const -> size_t;
+
+    constexpr auto at(uint16_t col, uint16_t row) -> TValue&;
+    constexpr auto at(uint16_t col, uint16_t row) const -> TValue const&;
+
+    constexpr auto begin() -> ValuesIterator;
+    constexpr auto begin() const -> ValuesConstIterator;
+
+    constexpr auto end() -> ValuesIterator;
+    constexpr auto end() const -> ValuesConstIterator;
+
+    constexpr auto operator+(Self const& other) const -> Self;
+    constexpr auto operator+=(Self const& other) -> Self&;
+
+    constexpr auto operator-(Self const& other) const -> Self;
+    constexpr auto operator-=(Self const& other) -> Self&;
+
+private:
+    constexpr TValue& _at(uint16_t col, uint16_t row);
+
+    ValueContainer values;
+};
+
+template <uint16_t _cols, uint16_t _rows, typename TValue>
+constexpr uint16_t Matrix<_cols, _rows, TValue>::columns() const
+{
+    return _cols;
+}
+
+template <uint16_t _cols, uint16_t _rows, typename TValue>
+constexpr uint16_t Matrix<_cols, _rows, TValue>::rows() const
+{
+    return _rows;
+}
+
+template <uint16_t _cols, uint16_t _rows, typename TValue>
+constexpr size_t Matrix<_cols, _rows, TValue>::size() const
+{
+    return _cols * _rows;
+}
+
+template <uint16_t _cols, uint16_t _rows, typename TValue>
+constexpr TValue& Matrix<_cols, _rows, TValue>::at(uint16_t col, uint16_t row)
+{
+    return _at(col, row);
+}
+
+template <uint16_t _cols, uint16_t _rows, typename TValue>
+constexpr TValue const& Matrix<_cols, _rows, TValue>::at(uint16_t col, uint16_t row) const
+{
+    return _at(col, row);
+}
+
+template <uint16_t _cols, uint16_t _rows, typename TValue>
+constexpr typename Matrix<_cols, _rows, TValue>::ValuesIterator
+Matrix<_cols, _rows, TValue>::begin()
+{
+    return values.begin();
+}
+
+template <uint16_t _cols, uint16_t _rows, typename TValue>
+constexpr typename Matrix<_cols, _rows, TValue>::ValuesConstIterator
+Matrix<_cols, _rows, TValue>::begin() const
+{
+    return values.begin();
+}
+
+template <uint16_t _cols, uint16_t _rows, typename TValue>
+constexpr typename Matrix<_cols, _rows, TValue>::ValuesIterator Matrix<_cols, _rows, TValue>::end()
+{
+    return values.end();
+}
+
+template <uint16_t _cols, uint16_t _rows, typename TValue>
+constexpr typename Matrix<_cols, _rows, TValue>::ValuesConstIterator
+Matrix<_cols, _rows, TValue>::end() const
+{
+    return values.end();
+}
+
+template <uint16_t _cols, uint16_t _rows, typename TValue>
+constexpr Matrix<_cols, _rows, TValue> Matrix<_cols, _rows, TValue>::
+operator+(Matrix<_cols, _rows, TValue> const& other) const
+{
+    auto result = Matrix{*this};
+    result += other;
+    return result;
+}
+
+template <uint16_t _cols, uint16_t _rows, typename TValue>
+constexpr Matrix<_cols, _rows, TValue>& Matrix<_cols, _rows, TValue>::
+operator+=(Matrix<_cols, _rows, TValue> const& other)
+{
+    auto i = this->values.begin();
+    auto j = other.values.begin();
+    for (; i != values.end(); ++i, ++j)
+    {
+        *i += *j;
+    }
+    return *this;
+}
+
+template <uint16_t _cols, uint16_t _rows, typename TValue>
+constexpr Matrix<_cols, _rows, TValue> Matrix<_cols, _rows, TValue>::
+operator-(Matrix<_cols, _rows, TValue> const& other) const
+{
+    auto result = Matrix{*this};
+    result -= other;
+    return result;
+}
+
+template <uint16_t _cols, uint16_t _rows, typename TValue>
+constexpr Matrix<_cols, _rows, TValue>& Matrix<_cols, _rows, TValue>::
+operator-=(Matrix<_cols, _rows, TValue> const& other)
+{
+    auto i = this->values.begin();
+    auto j = other.values.begin();
+    for (; i != values.end(); ++i, ++j)
+    {
+        *i -= *j;
+    }
+    return *this;
+}
+
+template <uint16_t _cols, uint16_t _rows, typename TValue>
+constexpr TValue& Matrix<_cols, _rows, TValue>::_at(uint16_t col, uint16_t row)
+{
+    assert(col < columns());
+    assert(row < rows());
+    return values[col + row * columns()];
+}
+} // namespace mino
+
---
+++ test/test_matrix.cpp
@@ -0,0 +1,57 @@
+#include "minomaly/common/matrix.hpp"
+#include "gtest/gtest.h"
+
+namespace
+{
+using namespace mino;
+
+TEST(Matrix, CanCreateInConstexprContext)
+{
+    constexpr auto matrix = Matrix<2, 3>{};
+    EXPECT_EQ(matrix.columns(), 2);
+    EXPECT_EQ(matrix.rows(), 3);
+}
+
+TEST(Matrix, CanAccessRightElements)
+{
+    /*
+     | 0  1 |
+     | 2  3 |
+     | 4  5 |
+    */
+    auto m = Matrix<2, 3>{{0, 1, 2, 3, 4, 5}};
+
+    EXPECT_EQ(m.at(0, 0), 0);
+    EXPECT_EQ(m.at(1, 0), 1);
+    EXPECT_EQ(m.at(0, 1), 2);
+    EXPECT_EQ(m.at(1, 1), 3);
+    EXPECT_EQ(m.at(0, 2), 4);
+    EXPECT_EQ(m.at(1, 2), 5);
+}
+
+TEST(Matrix, Addition)
+{
+    auto m1 = Matrix<2, 3>{{0, 1, 2, 3, 4, 5}};
+    auto m2 = Matrix<2, 3>{{5, 4, 3, 2, 1, 0}};
+
+    auto result = m1 + m2;
+
+    for (auto i = result.begin(); i != result.end(); ++i)
+    {
+        EXPECT_EQ(*i, 5);
+    }
+}
+
+TEST(Matrix, Subtraction)
+{
+    auto m = Matrix<2, 3>{{0, 1, 2, 3, 4, 5}};
+
+    auto result = m - m;
+
+    for (auto i = result.begin(); i != result.end(); ++i)
+    {
+        EXPECT_EQ(*i, 0);
+    }
+}
+} // namespace
+
